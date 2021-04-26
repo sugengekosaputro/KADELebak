1e204b33e8c63cabb5703e16f88e55c69de0ca9d
sugengekosaputro <sugengekosaputro96@gmail.com>
2021-04-26 09:52:00 +0700

    re-build, set to versionCode 10, versionName 1.1.0

--- app/src/main/java/com/inspektorat/kadelebak/Constant.java
+++ app/src/main/java/com/inspektorat/kadelebak/Constant.java
@@ -4,8 +4,7 @@ import java.io.Serializable;
 
 public class Constant {
 
-   public static final String BASE_URL = "http://id-3.hostddns.us:5536/kedelebakapi/";
-//    public static final String BASE_URL = "http://192.168.43.198:8080/kedelebakapi/";
+    public static final String BASE_URL = "http://36.92.143.137:8080/kedelebakapi/";
 
     public static final String SHARED_PREFERENCES_NAME = "MY_SHARED_PREFERENCES_DATA";
 
@@ -15,6 +14,19 @@ public class Constant {
     public static final String SECTION_ID = "sectionId";
     public static final String NAME = "name";
 
+    public static final String BORN_PLACE = "bornPlace";
+    public static final String DOB = "dob";
+    public static final String GENDER = "gender";
+    public static final String PHONE = "phone";
+    public static final String EMAIL = "email";
+    public static final String UNIT = "unit";
+    public static final String POSITION_ID = "positionId";
+    public static final String INSTITUTION_ID = "insitutionId";
+    public static final String VERIFIED = "verifed";
+    public static final String STATUS_VERIFIED = "statusVerifed";
+
+
+
     public static final String FORUM_ID = "forumId";
     public static final String COMPLAINT_ID = "complaintId";
 
@@ -23,4 +35,22 @@ public class Constant {
     public static final String SERIALIZABLE_COMPLAINT = "SERIALIZABLE_COMPLAINT";
 
     public static final String FORM_ERROR = "Harus diisi";
+
+    public static final int STATUS_VERIFIED_NULL = 1;
+    public static final int STATUS_VERIFIED_WAITING = 2;
+    public static final int STATUS_VERIFIED_REJECTED = 3;
+    public static final int STATUS_VERIFIED_ACCEPTED = 4;
+
+    public static final String STATUS_VERIFIED_1 = "SK belum ada";
+    public static final String STATUS_VERIFIED_2 = "Menunggu verifikasi";
+    public static final String STATUS_VERIFIED_3 = "Harap upload ulang SK";
+    public static final String STATUS_VERIFIED_4 = "SK Terverifikasi";
+
+    public static final String STATUS_NULL = "Belum Upload SK";
+    public static final String STATUS_WAITING = "Menunggu Verifikasi";
+    public static final String STATUS_REJECTED = "Verifikasi Ditolak";
+    public static final String STATUS_ACCEPTED = "Terverifikasi";
+
+    public static final String METHOD_POST = "POST";
+    public static final String METHOD_PUT = "PUT";
 }
